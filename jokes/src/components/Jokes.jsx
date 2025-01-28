8653b373562ed55f8fcb01626c274021931733be
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-28 11:36:03 +0100

    Update Jokes.jsx

--- jokes/src/components/Jokes.jsx
+++ jokes/src/components/Jokes.jsx
@@ -13,7 +13,7 @@ export default function Jokes(props) {
       {props.question && <p className="question">Setup: {props.question}</p>}
       {isShown === true && (
         <p className="punchline">Punchline: {props.punchline}</p>
-      )}
+      )} //Is not necessary to specify isShown === true, because isShown is a boolean
       <button onClick={toogleIsShown}>Show Punchline</button>
       <hr />
     </div>
